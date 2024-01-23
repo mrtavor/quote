090c2913a250725df0159d7767159916f129d708
Stas <131067974+mrtavor@users.noreply.github.com>
2024-01-23 12:19:08 +0100

    add .js file

---
+++ jsFiles/main.js
@@ -0,0 +1,5 @@
+const quote = document.getElementById('quote')
+const inputName = document.getElementById('nameField').value 
+const author = document.getElementById('author')
+const inputQuote = document.getElementById('quoteField')
+const doneBtn = document.getElementById('doneBtn')
